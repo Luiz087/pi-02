6146767df5d6db86af28456095d029500c7801e6
Luiz Fernando <luiz.s08@aluno.ifsc.edu.br>
2023-08-03 10:01:43 -0300

    Classe Pessoa

--- gspautomoveis/src/main/java/modelo/Pessoa.java
+++ gspautomoveis/src/main/java/modelo/Pessoa.java
@@ -1,5 +1,84 @@
 package modelo;
 
 public class Pessoa {
+	
+	private String nome;
+	private Long cpf;
+	private String usuario;
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private Long cep;
+	private String senha;
+	private Long telefone;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getCpf() {
+		return cpf;
+	}
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getBairro() {
+		return bairro;
+	}
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public Long getCep() {
+		return cep;
+	}
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public Long getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 }

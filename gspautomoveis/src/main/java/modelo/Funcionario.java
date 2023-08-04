2808f516dc5aa31d3d66ef0078fc855f152330d1
Luiz <luiz.s08@aluno.ifsc.edu.br>
2023-08-03 21:50:12 -0300

    delete admin, pessoa e add atributos func

--- gspautomoveis/src/main/java/modelo/Funcionario.java
+++ gspautomoveis/src/main/java/modelo/Funcionario.java
@@ -1,11 +1,65 @@
 package modelo;
 
-public class Funcionario extends Pessoa {
+public class Funcionario {
 
+	private String nome;
+	private Long cpf;
+	private String usuario;
+	private String senha;
+	private Long telefone;
+	private String email;
 	private Double salario;
 	private String funcao;
 	private Integer nivelCargo;
 
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Long getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Long telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 	public Double getSalario() {
 		return salario;
 	}

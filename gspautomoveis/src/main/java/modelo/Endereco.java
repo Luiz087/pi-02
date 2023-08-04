28de3b18f2792a8dbcb21ea6a01b3c58bf8ca1f9
Luiz <luiz.s08@aluno.ifsc.edu.br>
2023-08-03 21:36:24 -0300

    Criando classe endereço
    Criando classe endereço, retirando dados do endereço de pessoa e aplicando ctrl+f em algumas classes

---
+++ gspautomoveis/src/main/java/modelo/Endereco.java
@@ -0,0 +1,51 @@
+package modelo;
+
+public class Endereco {
+
+	private String rua;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private Long cep;
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Long getCep() {
+		return cep;
+	}
+
+	public void setCep(Long cep) {
+		this.cep = cep;
+	}
+
+}
--- gspautomoveis/src/main/java/modelo/Pagamento.java
+++ gspautomoveis/src/main/java/modelo/Pagamento.java
@@ -3,29 +3,34 @@ package modelo;
 import java.time.LocalDate;
 
 public class Pagamento {
-	
+
 	private Double comissao;
+
 	public Double getComissao() {
 		return comissao;
 	}
+
 	public void setComissao(Double comissao) {
 		this.comissao = comissao;
 	}
+
 	public String getFormapagamento() {
 		return formapagamento;
 	}
+
 	public void setFormapagamento(String formapagamento) {
 		this.formapagamento = formapagamento;
 	}
+
 	public LocalDate getDataPagamento() {
 		return dataPagamento;
 	}
+
 	public void setDataPagamento(LocalDate dataPagamento) {
 		this.dataPagamento = dataPagamento;
 	}
+
 	private String formapagamento;
-	private LocalDate dataPagamento; 
-	
-	
+	private LocalDate dataPagamento;
 
 }

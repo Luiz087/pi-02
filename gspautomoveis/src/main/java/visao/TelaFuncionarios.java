2e3b6dfa6c1bca9a5acfd71f965985bccc9c9fcb
Luiz Fernando <luiz.s08@aluno.ifsc.edu.br>
2023-08-08 11:57:25 -0300

    Tela Funcionários

--- gspautomoveis/src/main/java/visao/TelaFuncionarios.java
+++ gspautomoveis/src/main/java/visao/TelaFuncionarios.java
@@ -9,10 +9,19 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 public class TelaFuncionarios extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -50,48 +59,83 @@ public class TelaFuncionarios extends JFrame {
 		
 		JLabel lblNome = new JLabel("Nome:");
 		lblNome.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome.setBounds(480, 238, 133, 38);
+		lblNome.setBounds(399, 32, 133, 38);
 		contentPane.add(lblNome);
 		
 		JLabel lblCpf = new JLabel("CPF:");
 		lblCpf.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblCpf.setBounds(480, 287, 93, 38);
+		lblCpf.setBounds(439, 92, 93, 38);
 		contentPane.add(lblCpf);
 		
 		JLabel lblUsurio = new JLabel("Usuário:");
 		lblUsurio.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblUsurio.setBounds(480, 336, 163, 38);
+		lblUsurio.setBounds(862, 32, 163, 38);
 		contentPane.add(lblUsurio);
 		
 		JLabel lblSenha = new JLabel("Senha:");
 		lblSenha.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblSenha.setBounds(480, 385, 143, 38);
+		lblSenha.setBounds(775, 92, 143, 38);
 		contentPane.add(lblSenha);
 		
-		JLabel lblNome_4 = new JLabel("Nome:");
-		lblNome_4.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome_4.setBounds(480, 434, 133, 38);
-		contentPane.add(lblNome_4);
+		JLabel lblTelefone = new JLabel("Telefone:");
+		lblTelefone.setFont(new Font("Krona One", Font.PLAIN, 30));
+		lblTelefone.setBounds(1355, 32, 188, 38);
+		contentPane.add(lblTelefone);
 		
-		JLabel lblNome_5 = new JLabel("Nome:");
-		lblNome_5.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome_5.setBounds(480, 489, 133, 38);
-		contentPane.add(lblNome_5);
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setFont(new Font("Krona One", Font.PLAIN, 30));
+		lblEmail.setBounds(409, 152, 123, 38);
+		contentPane.add(lblEmail);
 		
-		JLabel lblNome_6 = new JLabel("Nome:");
-		lblNome_6.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome_6.setBounds(480, 550, 133, 38);
-		contentPane.add(lblNome_6);
+		JLabel lblDataDeNascimento = new JLabel("Data de Nascimento:");
+		lblDataDeNascimento.setFont(new Font("Krona One", Font.PLAIN, 30));
+		lblDataDeNascimento.setBounds(1241, 152, 415, 38);
+		contentPane.add(lblDataDeNascimento);
 		
-		JLabel lblNome_7 = new JLabel("Nome:");
-		lblNome_7.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome_7.setBounds(480, 620, 133, 38);
-		contentPane.add(lblNome_7);
+		JLabel lblCargo = new JLabel("Cargo:");
+		lblCargo.setFont(new Font("Krona One", Font.PLAIN, 30));
+		lblCargo.setBounds(1152, 92, 133, 38);
+		contentPane.add(lblCargo);
 		
-		JLabel lblNome_8 = new JLabel("Nome:");
-		lblNome_8.setFont(new Font("Krona One", Font.PLAIN, 30));
-		lblNome_8.setBounds(480, 682, 133, 38);
-		contentPane.add(lblNome_8);
+		textField = new JTextField();
+		textField.setBounds(542, 32, 292, 38);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(542, 92, 196, 38);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(1035, 32, 292, 38);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(1553, 32, 309, 38);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(928, 92, 196, 38);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(542, 152, 675, 38);
+		contentPane.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(1666, 152, 196, 38);
+		contentPane.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(1295, 92, 567, 38);
+		contentPane.add(textField_7);
 	}
 
 }

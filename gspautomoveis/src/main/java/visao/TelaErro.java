59e7eeeac23f3e17172f89a2498a284f495ff805
luis <luis.g2005@aluno.ifsc.edu.br>
2023-08-15 14:12:06 -0300

    telas de mensagem

--- gspautomoveis/src/main/java/visao/TelaErro.java
+++ gspautomoveis/src/main/java/visao/TelaErro.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import java.awt.Color;
 
 public class TelaErro extends JFrame {
 
@@ -33,9 +39,29 @@ public class TelaErro extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.inactiveCaptionBorder);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
+		setUndecorated(true);
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("X");
+		btnNewButton.setBackground(SystemColor.inactiveCaptionBorder);
+		btnNewButton.setForeground(new Color(255, 0, 0));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 24));
+		btnNewButton.setBounds(373, 0, 89, 23);
+		btnNewButton.setBorder(null);
+		btnNewButton.setFocusPainted(false);
+
+		contentPane.add(btnNewButton);
+		
+		
 	}
 
 }

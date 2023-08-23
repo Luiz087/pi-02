2782a540651f149292bc99a5f9c7176f5486dd5c
eric.o26 <eric.o26@aluno.ifsc.edu.br>
2023-08-23 20:35:04 -0300

    comentei o erro

--- gspautomoveis/src/main/java/visao/TelaPrincipal.java
+++ gspautomoveis/src/main/java/visao/TelaPrincipal.java
@@ -271,7 +271,7 @@ public class TelaPrincipal extends JFrame {
 		text_nome.setBounds(546, 159, 239, 35);
 		contentPane.add(text_nome);
 		text_nome.setColumns(10);
-		text_nome.setText();
+		/*text_nome.setText();*/
 		
 		
 		JLabel lblNewLabel_nome = new JLabel("Nome:");

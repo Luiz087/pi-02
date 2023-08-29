e37f32dfe4a20b7a90c5ad645bb36e564b23f815
omaffezzolli <joao.l09@aluno.ifsc.edu.br>
2023-08-29 11:11:48 -0300

    tela historico

---
+++ gspautomoveis/src/main/java/visao/TelaHistoricoFornecedores.java
@@ -0,0 +1,132 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+
+public class TelaHistoricoFornecedores extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaHistoricoFornecedores frame = new TelaHistoricoFornecedores();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaHistoricoFornecedores() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1920, 1080);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel_4_1_1_1_1_1_1 = new JLabel("Sair");
+		lblNewLabel_4_1_1_1_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1_1_1_1_1.setFont(new Font("Krona One", Font.PLAIN, 24));
+		lblNewLabel_4_1_1_1_1_1_1.setBounds(63, 936, 93, 35);
+		contentPane.add(lblNewLabel_4_1_1_1_1_1_1);
+		
+		JLabel lblNewLabel_3 = new JLabel("GSP");
+		lblNewLabel_3.setForeground(Color.WHITE);
+		lblNewLabel_3.setFont(new Font("Krona One", Font.PLAIN, 57));
+		lblNewLabel_3.setBounds(63, 43, 166, 72);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setBounds(63, 141, 175, 190);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2_1_2 = new JLabel("");
+		lblNewLabel_2_1_2.setBounds(17, 410, 40, 35);
+		contentPane.add(lblNewLabel_2_1_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("Veículos");
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4.setBounds(63, 410, 265, 35);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("");
+		lblNewLabel_2_1.setBounds(17, 490, 40, 35);
+		contentPane.add(lblNewLabel_2_1);
+		
+		JLabel lblNewLabel_4_1 = new JLabel("Configurações");
+		lblNewLabel_4_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4_1.setBounds(63, 490, 265, 35);
+		contentPane.add(lblNewLabel_4_1);
+		
+		JLabel lblNewLabel_2_1_1 = new JLabel("");
+		lblNewLabel_2_1_1.setBounds(17, 570, 40, 35);
+		contentPane.add(lblNewLabel_2_1_1);
+		
+		JLabel lblNewLabel_4_1_1 = new JLabel("Funcionários");
+		lblNewLabel_4_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4_1_1.setBounds(63, 570, 265, 35);
+		contentPane.add(lblNewLabel_4_1_1);
+		
+		JLabel lblNewLabel_2_1_1_1 = new JLabel("");
+		lblNewLabel_2_1_1_1.setBounds(17, 645, 40, 35);
+		contentPane.add(lblNewLabel_2_1_1_1);
+		
+		JLabel lblNewLabel_4_1_1_1 = new JLabel("Histórico");
+		lblNewLabel_4_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1_1.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4_1_1_1.setBounds(63, 645, 265, 35);
+		contentPane.add(lblNewLabel_4_1_1_1);
+		
+		JLabel lblNewLabel_2_1_1_1_1 = new JLabel("");
+		lblNewLabel_2_1_1_1_1.setBounds(17, 717, 40, 35);
+		contentPane.add(lblNewLabel_2_1_1_1_1);
+		
+		JLabel lblNewLabel_4_1_1_1_1 = new JLabel("Fornecedores");
+		lblNewLabel_4_1_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1_1_1.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4_1_1_1_1.setBounds(63, 717, 265, 35);
+		contentPane.add(lblNewLabel_4_1_1_1_1);
+		
+		JLabel lblNewLabel_2_1_1_1_1_1 = new JLabel("");
+		lblNewLabel_2_1_1_1_1_1.setBounds(17, 800, 40, 35);
+		contentPane.add(lblNewLabel_2_1_1_1_1_1);
+		
+		JLabel lblNewLabel_4_1_1_1_1_1 = new JLabel("Sistema");
+		lblNewLabel_4_1_1_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_4_1_1_1_1_1.setFont(new Font("Krona One", Font.PLAIN, 26));
+		lblNewLabel_4_1_1_1_1_1.setBounds(63, 800, 265, 35);
+		contentPane.add(lblNewLabel_4_1_1_1_1_1);
+		
+		JLabel lblNewLabel_2_1_1_1_1_1_1 = new JLabel("");
+		lblNewLabel_2_1_1_1_1_1_1.setIcon(new ImageIcon(TelaHistoricoFornecedores.class.getResource("/visao/imagens/sair.png")));
+		lblNewLabel_2_1_1_1_1_1_1.setBounds(17, 937, 40, 35);
+		contentPane.add(lblNewLabel_2_1_1_1_1_1_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(TelaHistoricoFornecedores.class.getResource("/visao/imagens/bglateral.png")));
+		lblNewLabel.setBounds(-14, -47, 361, 1141);
+		contentPane.add(lblNewLabel);
+	}
+}
